b3648cef6a66de5d6150f952c2d6005bbef3b099
Dipanshi Digga <dipanshi600@gmail.com>
2020-10-07 10:41:51 +0530

    first deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001";
+const ENDPOINT = "https://say-hey-server.herokuapp.com";
 const socket = socketIOClient(ENDPOINT);
 
 function App(props) {
